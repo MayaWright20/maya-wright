6a53c911358e319e5a759058fdc416cb3baf44b3
Maya <mayagwright20@hotmail.com>
2025-03-27 21:38:55 +0000

    Add 3d projects text

---
+++ src/app/components/r3F/models/3D-text/projects-3D-text.tsx
@@ -0,0 +1,31 @@
+import { Center, Text3D } from '@react-three/drei';
+import fontUrl from '../../../../fonts/Permanent_Marker_Regular.json';
+import { COLORS } from '@/app/constants/colors';
+import { usePersistStore } from '@/app/store/store';
+
+const textOptions = {
+  size: 0.15,
+  height: 0.02,
+  curveSegments: 12,
+  bevelEnabled: true,
+  bevelThickness: 0.05,
+  bevelSize: 0,
+  bevelOffset: 0,
+  bevelSegments: 5,
+};
+
+export default function Projects3DText() {
+  const { isDaylightTheme } = usePersistStore();
+  return (
+    <Center top>
+      <Text3D
+        font="../../../../fonts/Permanent_Marker_Regular.json"
+        {...textOptions}
+      >
+        Projects
+        {isDaylightTheme && <meshPhongMaterial color={COLORS.dark_green} />}
+        {!isDaylightTheme && <meshNormalMaterial />}
+      </Text3D>
+    </Center>
+  );
+}
