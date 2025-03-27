9aece22ce05809bfe0c911946271b16bd261b8e5
Maya <mayagwright20@hotmail.com>
2025-03-27 21:48:53 +0000

    Fix 3d text

--- src/app/components/r3F/models/3D-text/projects-3D-text.tsx
+++ src/app/components/r3F/models/3D-text/projects-3D-text.tsx
@@ -1,5 +1,4 @@
 import { Center, Text3D } from '@react-three/drei';
-import fontUrl from '../../../../fonts/Permanent_Marker_Regular.json';
 import { COLORS } from '@/app/constants/colors';
 import { usePersistStore } from '@/app/store/store';
 
@@ -18,10 +17,7 @@ export default function Projects3DText() {
   const { isDaylightTheme } = usePersistStore();
   return (
     <Center top>
-      <Text3D
-        font="../../../../fonts/Permanent_Marker_Regular.json"
-        {...textOptions}
-      >
+      <Text3D font="/Inter_Bold.json" {...textOptions}>
         Projects
         {isDaylightTheme && <meshPhongMaterial color={COLORS.dark_green} />}
         {!isDaylightTheme && <meshNormalMaterial />}
