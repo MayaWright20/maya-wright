576dc61d1497574bdcf20f148f4235c69cffecc5
Maya <mayagwright20@hotmail.com>
2025-03-27 17:32:29 +0000

    Refactor

--- src/app/components/r3F/models/camera-path/camera-path.jsx
+++ src/app/components/r3F/models/camera-path/camera-path.jsx
@@ -1,28 +1,23 @@
-import { useRef, forwardRef, useState } from 'react';
+import { forwardRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { MotionPathControls, useMotion } from '@react-three/drei';
 import * as CURVES from './curves';
 import Face from '../face/face.jsx';
 
 export function CameraPath() {
-  const poi = useRef();
-  const motionRef = useRef();
-  const [path] = useState('Circle');
+  const [path] = useState('Infinity');
   const Curve = CURVES[path];
   return (
-    <>
-      <MotionPathControls
-        object={true ? null : motionRef}
-        focus={poi}
-        debug={true}
-        damping={0.2}
-        focusDamping={0.15}
-      >
-        <Curve />
-        <Loop />
-      </MotionPathControls>
-      <Sticker position={[1, 0, 1]} scale={2} ref={poi} />
-    </>
+    <MotionPathControls
+      focus={[0, 0, 0]}
+      debug={true}
+      damping={0.2}
+      focusDamping={0.15}
+    >
+      <Curve />
+      <Loop />
+      <Sticker position={[0, 0, 0]} scale={1} />
+    </MotionPathControls>
   );
 }
 
@@ -31,12 +26,6 @@ function Loop({ factor = 0.05 }) {
   useFrame((state, delta) => (motion.current += Math.min(0.1, delta) * factor));
 }
 
-const Sticker = forwardRef(({ url, ...props }, ref) => {
-  return (
-    <>
-      <mesh ref={ref} {...props}>
-        <Face />
-      </mesh>
-    </>
-  );
+const Sticker = forwardRef(() => {
+  return <Face />;
 });
