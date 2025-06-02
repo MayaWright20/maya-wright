94a813800ae85f017aee46194a55759bcd2daa0e
Maya <mayagwright20@hotmail.com>
2025-06-02 19:22:25 +0100

    Change to darker lights

--- src/r3F/models/lights/directional-lights.tsx
+++ src/r3F/models/lights/directional-lights.tsx
@@ -7,47 +7,47 @@ const LIGHTS_ATTRIBUTES: {
   {
     position: [15, -30, 15],
     intensity: 2,
-    color: 'rgb(83, 198, 66)',
+    color: 'rgb(45, 106, 36)',
   },
   {
     position: [10, -20, 15],
     intensity: 2,
-    color: 'rgb(132, 0, 255)',
+    color: 'rgb(69, 0, 133)',
   },
   {
     position: [-20, -50, -25],
     intensity: 2,
-    color: 'rgb(0, 255, 123)',
+    color: 'rgb(1, 127, 62)',
   },
   {
     position: [10, -12, -25],
     intensity: 2,
-    color: 'rgb(0, 251, 255)',
+    color: 'rgb(1, 99, 101)',
   },
   {
     position: [-10, -10, -10],
     intensity: 3,
-    color: 'rgb(255, 0, 0)',
+    color: 'rgb(132, 0, 0)',
   },
   {
     position: [10, 1000, 20],
     intensity: 2,
-    color: 'rgb(195, 9, 212)',
+    color: 'rgb(91, 3, 99)',
   },
   {
     position: [-20, 0, 10],
     intensity: 1.4,
-    color: 'rgb(255, 0, 153)',
+    color: 'rgb(128, 1, 77)',
   },
   {
     position: [0, 0, 1000],
     intensity: 1,
-    color: 'rgb(14, 238, 167)',
+    color: 'rgb(8, 129, 91)',
   },
   {
     position: [10, -10, 0],
     intensity: 1,
-    color: 'rgb(8, 245, 83)',
+    color: 'rgb(2, 99, 33)',
   },
 ];
 
