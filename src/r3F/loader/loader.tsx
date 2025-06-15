672ef626ba255b5737997d6a7c77f83716c8ef41
Maya <mayagwright20@hotmail.com>
2025-06-15 20:52:22 +0100

    Add loaded nighttime background

--- src/r3F/loader/loader.tsx
+++ src/r3F/loader/loader.tsx
@@ -2,6 +2,22 @@ import { Html, useProgress } from '@react-three/drei';
 
 export default function Loader() {
   const { progress } = useProgress();
+  // {progress.toFixed(1)} % loaded
 
-  return <Html center>{progress.toFixed(1)} % loaded</Html>;
+  return (
+    <Html center>
+      <div
+        style={{
+          backgroundColor: 'black',
+          height: '100vh',
+          width: '100vw',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <h1 style={{ color: 'whitesmoke' }}>{progress.toFixed(1)} % loaded</h1>
+      </div>
+    </Html>
+  );
 }
