5c35007821cfec26a5d970202c9710ddca0836a4
mayagwright20 <mayagwright20@hotmail.com>
2025-10-30 14:00:34 +0000

    Add glow of mouse

--- src/r3F/models/scene/scene.tsx
+++ src/r3F/models/scene/scene.tsx
@@ -1,52 +1,75 @@
-'use client';
+"use client";
 
-import { Canvas } from '@react-three/fiber';
-import { Suspense, useContext } from 'react';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { Suspense, useContext, useRef } from "react";
 import {
   Cloud,
   Clouds,
   Grid,
-  Html,
   OrbitControls,
   Sky,
   Stars,
-  useProgress,
-} from '@react-three/drei';
-import DirectionalLights from '../lights/directional-lights';
-import Face from '../face/face';
-import * as THREE from 'three';
-import PixelatedHeartsInstances from '../heart/pixelated-hearts-instances';
-import { usePersistStore } from '@/store/store';
-import { CameraPath } from '../camera-path/camera-path';
-import { IsCameraMotionPathsControlContext } from '@/context/r3f/isCameraMotionPathsControlContext';
-import Projects3DText from '../3D-text/projects-3D-text';
-import Loader from '@/r3F/loader/loader';
-import { COLORS } from '@/constants/colors';
+} from "@react-three/drei";
+import DirectionalLights from "../lights/directional-lights";
+import Face from "../face/face";
+import * as THREE from "three";
+import { usePersistStore } from "@/store/store";
+import { CameraPath } from "../camera-path/camera-path";
+import { IsCameraMotionPathsControlContext } from "@/context/r3f/isCameraMotionPathsControlContext";
+import Projects3DText from "../3D-text/projects-3D-text";
+import Loader from "@/r3F/loader/loader";
+import { COLORS } from "@/constants/colors";
 
 interface Props {
   hearts: boolean;
 }
 
+function Model(props) {
+  const group = useRef<any>();
+  const light = useRef<any>();
+
+  useFrame((state, delta) => {
+    easing.dampE(
+      group.current.rotation,
+      [0, -state.pointer.x * (Math.PI / 10), 0],
+      1.5,
+      delta
+    );
+    easing.damp3(
+      group.current.position,
+      [0, -5.5, 1 - Math.abs(state.pointer.x)],
+      1,
+      delta
+    );
+    easing.damp3(
+      light.current.position,
+      [state.pointer.x * 12, 0, 8 + state.pointer.y * 4],
+      0.2,
+      delta
+    );
+  });
+  return (
+    <group ref={group} {...props}>
+      <spotLight
+        angle={0.9}
+        penumbra={0.9}
+        ref={light}
+        castShadow
+        intensity={70}
+        shadow-mapSize={1024}
+        shadow-bias={-0.001}
+      >
+        <orthographicCamera
+          attach="shadow-camera"
+          // args={[-10, 10, -10, 10, 0.1, 50]}
+        />
+      </spotLight>
+    </group>
+  );
+}
+
 export default function Scene({ hearts }: Props) {
-  // scenes to create:
-  // ____________________
-  // emotions
-  // pully face
-  // shock with milk pouring down
-  // angry
-  // wink with tongue
-  // kiss
-
-  // Themes
-  // ____________________
-  // 3D red and blue
-  // gold
-  // white milk
-  // anime
-  // playdoe
-  // love - red floating foil balloons / fluffy red baloons/ text
-  // colorful mixblendmode difference
-
   const { isDaylightTheme, autoRotateModel } = usePersistStore();
   const isCameraMotionPath = useContext(IsCameraMotionPathsControlContext);
 
@@ -55,10 +78,10 @@ export default function Scene({ hearts }: Props) {
       gl={{ antialias: true, alpha: true }}
       dpr={[1, 1.5]}
       style={{
-        overflow: 'visible',
-        position: 'absolute',
-        height: '100%',
-        width: '100%',
+        overflow: "visible",
+        position: "absolute",
+        height: "100%",
+        width: "100%",
         backgroundColor: COLORS.blender_grey,
         zIndex: -9999,
       }}
@@ -75,6 +98,7 @@ export default function Scene({ hearts }: Props) {
         {isCameraMotionPath && <CameraPath />}
         {isCameraMotionPath && <Projects3DText />}
         <Face />
+        <Model />
         <Sky
           distance={450000}
           sunPosition={[0, isDaylightTheme ? 1 : 0, 0]}
@@ -89,7 +113,6 @@ export default function Scene({ hearts }: Props) {
             infiniteGrid
           />
         )}
-
         <Clouds material={THREE.MeshBasicMaterial}>
           <Cloud
             segments={40}
